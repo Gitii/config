09749a182cce34c526f2179ad0ff5f5cdf85496f
Gitii <Gitii@users.noreply.github.com>
2022-02-20 14:03:39 +0100

    Fixed bootstrap script

--- templates/.my-config/bootstrap.ps1
+++ templates/.my-config/bootstrap.ps1
@@ -11,10 +11,40 @@ function Get-ScriptFolder() {
 function New-TemporaryDirectory {
     $parent = [System.IO.Path]::GetTempPath()
     [string] $name = [System.Guid]::NewGuid()
-    $path = Join-Path $parent $name
-    New-Item -ItemType Directory -Path $path
+    [string] $path = Join-Path $parent $name
+    New-Item -ItemType Directory -Path $path | Out-Null
 
     return $path
+}
+
+function Extract-Zip($ArchivePath, $TargetDirectory) {
+    Add-Type -Assembly "System.IO.Compression.Filesystem"
+    $ar = [System.IO.Compression.ZipFile]::OpenRead($tempFile.FullName)
+
+    $ar.Entries | ? { $_.Name -ne "" } | % {
+        $entry = $_
+
+        $directory = Split-Path $entry.FullName
+        $fullTargetDirectory = Join-Path $TargetDirectory $directory
+
+        New-Item -ItemType Directory $fullTargetDirectory -Force | Out-Null
+
+        $fullTargetFilePath = Join-Path $fullTargetDirectory $entry.Name
+
+        Write-Debug "Extracting $($entry.Name) to $fullTargetFilePath"
+
+        $stream = New-Object -TypeName System.IO.FileStream -ArgumentList @($fullTargetFilePath, [System.IO.FileMode]::OpenOrCreate)
+
+        $entryStream = $entry.Open()
+
+        $entryStream.CopyTo($stream)
+
+        $stream.Close()
+        $entryStream.Close()
+    }
+
+    $ar.Dispose()
+
 }
 
 function Extract-Config($TargetDirectory) {
@@ -28,17 +58,17 @@ function Extract-Config($TargetDirectory) {
         Invoke-WebRequest -Uri $url -OutFile $tempFile.FullName
         
         Write-Host "Extracting to $tempFolder..."
-        Expand-Archive $tempFile.FullName -DestinationPath $tempFolder -Force
+        Extract-Zip -ArchivePath $tempFile.FullName -TargetDirectory $tempFolder
 
-        $templateFolder = Join-Path $tempFolder "templates"
+        $templateFolder = Join-Path $tempFolder "config-main" "templates"
 
         Write-Host "Copying templates to $TargetDirectory"
-        Copy-Item -Path $templateFolder -Recurse -Destination $TargetDirectory
+        Get-ChildItem -Path $templateFolder -Force | Copy-Item -Recurse -Destination $TargetDirectory -Force
 
     } finally {
         $tempFile.Delete()
 
-        Remove-Item $tempFolder -Recurse -Force -WhatIf
+        Remove-Item $tempFolder -Recurse -Force
     }
 }
 
